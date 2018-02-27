cfdf415c927461a23118ec87d3af7af9218e50f9
kaen98 <846969893@qq.com>
2018-02-27 02:06:05 +0000

    Integrated administrator

--- storage/framework/views/710bcf0e3348cb9f7a5eb864aea95996ac7ad282.php
+++ storage/framework/views/710bcf0e3348cb9f7a5eb864aea95996ac7ad282.php
@@ -58,6 +58,14 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+                            <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('manage_contents')): ?>
+                                <li>
+                                    <a href="<?php echo e(url(config('administrator.uri'))); ?>">
+                                        <span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span>
+                                        管理后台
+                                    </a>
+                                </li>
+                            <?php endif; ?>
                             <li>
                                 <a href="<?php echo e(route('users.show', Auth::id())); ?>">
                                     <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
