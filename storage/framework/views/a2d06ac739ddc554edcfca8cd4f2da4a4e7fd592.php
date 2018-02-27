4e11125279facb4f5a0defbbd2d5e84a8711a719
kaen98 <846969893@qq.com>
2018-02-27 04:30:47 +0000

    Admin can manage site settings

--- storage/framework/views/a2d06ac739ddc554edcfca8cd4f2da4a4e7fd592.php
+++ storage/framework/views/a2d06ac739ddc554edcfca8cd4f2da4a4e7fd592.php
@@ -8,8 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-    <title><?php echo $__env->yieldContent('title', 'LaraBBS'); ?> - Laravel 进阶教程</title>
-    <meta name="description" content="<?php echo $__env->yieldContent('description', 'LaraBBS 爱好者社区'); ?>" />
+    <title><?php echo $__env->yieldContent('title', 'LaraBBS'); ?> - <?php echo e(setting('site_name', 'Laravel 进阶教程')); ?></title>
+    <meta name="description" content="<?php echo $__env->yieldContent('description', setting('seo_description', 'LaraBBS 爱好者社区。')); ?>" />
+    <meta name="keyword" content="<?php echo $__env->yieldContent('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛')); ?>" />
     <!-- Styles -->
     <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
     <?php echo $__env->yieldContent('styles'); ?>
