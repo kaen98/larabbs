b3c59514f6baf5923203edb253f7f31d0c8a9f88
kaen98 <846969893@qq.com>
2018-03-02 02:07:01 +0000

    User default avatar after signup

---
+++ storage/framework/views/f2017439bb4f2c840a07c1a2663a2a386bb60024.php
@@ -0,0 +1,93 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">用户注册</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="<?php echo e(route('register')); ?>">
+                        <?php echo e(csrf_field()); ?>
+
+
+                        <div class="form-group<?php echo e($errors->has('name') ? ' has-error' : ''); ?>">
+                            <label for="name" class="col-md-4 control-label">用户名</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="<?php echo e(old('name')); ?>" required autofocus>
+
+                                <?php if($errors->has('name')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('name')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group<?php echo e($errors->has('email') ? ' has-error' : ''); ?>">
+                            <label for="email" class="col-md-4 control-label">E-Mail 地址</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="<?php echo e(old('email')); ?>" required>
+
+                                <?php if($errors->has('email')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('email')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group<?php echo e($errors->has('password') ? ' has-error' : ''); ?>">
+                            <label for="password" class="col-md-4 control-label">密 码</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                <?php if($errors->has('password')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('password')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">重复密码</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group <?php echo e($errors->has('captcha') ? ' has-error' : ''); ?>">
+                            <label for="captcha" class="col-md-4 control-label">验证码</label>
+
+                            <div class="col-md-6">
+                                <input id="captcha" class="form-control" name="captcha" >
+
+                                <img class="thumbnail captcha" src="<?php echo e(captcha_src('flat')); ?>" onclick="this.src='/captcha/flat?'+Math.random()" title="点击图片重新获取验证码">
+
+                                <?php if($errors->has('captcha')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('captcha')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    注册 <i class="glyphicon glyphicon-arrow-right"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
