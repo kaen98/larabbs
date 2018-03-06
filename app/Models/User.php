ba8c65f8d86d7d9f4063c8792583203b04da0946
kaen98 <846969893@qq.com>
2018-03-06 15:25:58 +0800

    social authorizations

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'introduction', 'avatar', 'phone'
+        'name', 'email', 'password', 'introduction', 'avatar', 'phone', 'weixin_openid', 'weixin_unionid'
     ];
 
     /**
