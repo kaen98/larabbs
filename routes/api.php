795c27bc4a575f57aa4fabe1c12687465d143100
kaen98 <846969893@qq.com>
2018-03-14 18:24:44 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Http\Request;
 // });
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
 ], function($api){
     $api->group([
         'middleware' => 'api.throttle',
@@ -52,6 +53,13 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+
+        // 需要token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
     });
 
 
