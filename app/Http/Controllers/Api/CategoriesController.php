06e965f5e0b0fd3a7086bd597def1b19b6181dd9
kaen98 <846969893@qq.com>
2018-03-26 15:52:17 +0800

    categories index

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Transformers\CategoryTransformer;
 
 class CategoriesController extends Controller
 {
