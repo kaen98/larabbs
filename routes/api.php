32f72b0e728c945784373ae5b6d9050d26c0a370
kaen98 <846969893@qq.com>
2018-04-27 16:40:22 +0800

    replies store

--- routes/api.php
+++ routes/api.php
@@ -94,6 +94,10 @@ $api->version('v1', [
             // 删除话题
             $api->delete('topics/{topic}', 'TopicsController@destroy')
                 ->name('api.topics.destroy');
+
+            // 发布回复
+            $api->post('topics/{topic}/replies', 'RepliesController@store')
+                ->name('api.topics.replies.store');
         });
     });
 
