0d501c131949fad1b0bbc4e8dd722ad73d237e91
kaen98 <846969893@qq.com>
2018-05-05 17:28:49 +0800

    links index

--- routes/api.php
+++ routes/api.php
@@ -69,6 +69,9 @@ $api->version('v1', [
         // 某个用户的回复列表
         $api->get('users/{user}/replies', 'RepliesController@userIndex')
             ->name('api.users.replies.index');
+        // 资源推荐
+        $api->get('links', 'LinksController@index')
+            ->name('api.links.index');
 
         $api->get('topics/{topic}', 'TopicsController@show')
             ->name('api.topics.show');
